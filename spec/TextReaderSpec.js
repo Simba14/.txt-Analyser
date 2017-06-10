2a0ed9b4e5fa8ed474d3eaa2dcde97714f8fb4ed
Sean Blundell <seanblundell@gmail.com>
2017-06-10 15:42:58 +0100

    Pass test: it initializes with an empty word list hash

--- spec/TextReaderSpec.js
+++ spec/TextReaderSpec.js
@@ -2,7 +2,7 @@ describe("TextReader", function() {
   var textReader;
 
   beforeEach(function() {
-    textReader = new textReader();
+    textReader = new TextReader();
   });
 
   it('initializes with an empty word list hash', function() {
