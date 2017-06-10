db879a59358d30cab67ce68ed3f58eea7125e17d
Sean Blundell <seanblundell@gmail.com>
2017-06-10 17:19:39 +0100

    Write test: retrieves all the words from a text and populates the wordList array

--- spec/TextReaderSpec.js
+++ spec/TextReaderSpec.js
@@ -1,11 +1,21 @@
-describe("TextReader", function() {
+describe('TextReader', function() {
   var textReader;
 
   beforeEach(function() {
     textReader = new TextReader();
   });
 
-  it('initializes with an empty wordList array', function() {
-    expect(textReader.wordList).toEqual([]);
+  describe('initialization', function() {
+    it('initializes with an empty wordList array', function() {
+      expect(textReader.wordList).toEqual([]);
+    });
+  })
+
+  describe('#populateWordList', function() {
+    it('retrieves all the words from a text and populates the wordList array', function() {
+      text = 'Meet the Meerkats';
+      text.populateWordList(text);
+      expect(textReader.wordList).toEqual(['Meet', 'the', 'Meerkats']);
+    });
   });
 });
