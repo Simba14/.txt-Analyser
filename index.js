263457bbaaaff97d1756e3ae7d6f2565c1cd83a5
Sean Blundell <seanblundell@gmail.com>
2017-06-11 15:08:07 +0100

    Create index.html and link with node server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var path = require('path');
+app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!')
