14401ded1f8c221e38e4349fc53e29c278b7ce17
Sean Blundell <seanblundell@gmail.com>
2017-06-11 14:08:02 +0100

    Write test: stores file passed as argmument

--- spec/FileCheckerSpec.js
+++ spec/FileCheckerSpec.js
@@ -1,7 +1,13 @@
-describe('FileChecker', function() {
-  let fileChecker;
+describe('FileHandler', function() {
+  let fileHandler;
 
   beforeEach(function() {
-    fileChecker = new FileChecker();
+    fileHandler = new FileHandler(sample_text.txt);
   });
+
+  describe('initialization', function() {
+    it('stores file passed as argmument', function() {
+      expect(this.file).toEqual(sample_text.txt);
+    })
+  })
 })
